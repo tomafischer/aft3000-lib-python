af964f0b2ef2aa0c806c6af39a4b19afa03de7d9
Tom <tom.a.fischer@gmail.com>
2022-05-16 17:32:40 -0400

    added parse date strings

--- aft3000_lib/dates_lib.py
+++ aft3000_lib/dates_lib.py
@@ -1,4 +1,5 @@
 from datetime import datetime, timedelta
+from xmlrpc.client import Boolean
 from pytz import timezone
 
 def date_monday_of_week(day_of_week: datetime = datetime.now()):
@@ -11,7 +12,28 @@ def date_monday_of_week(day_of_week: datetime = datetime.now()):
     monday_morning = datetime(dt.year,dt.month, dt.day, 0, 0, 0)
     return monday_morning
 
+def format_iso_no_tz(dt: datetime, microseconds= False ) -> str:
+  """
+  Formats the datetime object into iso format
+  """
+  if microseconds:
+    return dt.strftime("%Y-%m-%d %H:%M:%S:%f")
+  else:
+    return dt.strftime("%Y-%m-%d %H:%M:%S")
+
+def dt_fromisoformat(dt_str: str, remove_tz: Boolean = False) -> datetime:
+    """
+    Parse Iso datetime to datetime and remove txinfo if required
+    """
+    dt=  datetime.fromisoformat(dt_str)
+    if remove_tz:
+      return dt.replace(tzinfo= None)
+    return dt
+
 def CST_now() -> datetime: 
+  """
+  Returns the curren timestamp for CST
+  """
   tz = timezone('US/Central')
   ts =datetime.now(tz) #+ timedelta(hours = -1)
   ts = ts.replace(tzinfo=None)
--- tests/test_dates.py
+++ tests/test_dates.py
@@ -1,6 +1,6 @@
 import pytest 
 from datetime import datetime, timedelta
-from aft3000_lib.dates_lib import date_monday_of_week
+from aft3000_lib.dates_lib import date_monday_of_week, format_iso_no_tz, dt_fromisoformat
 
 @pytest.mark.parametrize('midweek',[[datetime(2021,10,4),datetime(2021,10,4,0,0,0)],
   [datetime(2021,10,4),datetime(2021,10,4,0,0,0)],
@@ -16,5 +16,20 @@ def test_date_monday_of_week(midweek):
     monday = date_monday_of_week(midweek[0])
     #print(f"{monday} - {midweek[1]}")
     assert monday == midweek[1]
+
+@pytest.mark.dates
+def test_format_iso_no_tz():
+  # arrange
+  dt = datetime(2020,5,4,1,2,1,123456)
+
+  # act , assert
+  assert format_iso_no_tz(dt, microseconds=True) == '2020-05-04 01:02:01:123456'
+  assert format_iso_no_tz(dt, ) == '2020-05-04 01:02:01'
+
+@pytest.mark.dates
+def test_dt_fromisoformat():
+  assert dt_fromisoformat('2020-05-04 01:02:01:123456') == datetime(2020,5,4,1,2,1,123456)
+  assert dt_fromisoformat('2020-05-04T01:02:01:123456+04:00', remove_tz=True) == datetime(2020,5,4,1,2,1,123456) , "remove txinfo"
+
 
 
